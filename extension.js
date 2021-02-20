880943606ef66541501c44882b349879f3a06372
R1dge <rr1dge939@gmail.com>
2021-02-20 22:50:36 +0900

    1.0.4

--- extension.js
+++ extension.js
@@ -29,7 +29,7 @@ function activate(context) {
 		let url = new RegExp('https.*')
 		let ruby = new RegExp('\\x7c')
 
-		console.log(text.length)
+		// console.log(text.length)
 
 		if(url.test(text)==true){	// URLの場合、専用形式に置き換える
 			let re_id = new RegExp('(?<=https://)[0-9]*')
@@ -40,12 +40,25 @@ function activate(context) {
 			editor.edit(builder => builder.replace(selection, img)) // 選択してた文字列を、newTextに置き換え（カッコを追加）	
 	
 		}else{
-			if(ruby.test(text)==true){	// ルビ振り済み（読みなし）の場合、除去
-				let re_rubied = new RegExp('(?<=\\x7c).*(?=《》)')
-				let rubied = text.match(re_rubied)[0]
-				editor.edit(builder => builder.replace(selection, rubied)) // 選択してた文字列を、newTextに置き換え（カッコを追加）
+			if(ruby.test(text)==true){	// ルビ振り済み（読みなし）の場合（選択文字列が記号|から始まる場合）
+				// 読み仮名のカッコ内が空白の場合
+				let re_rubied = new RegExp('(?<=\\x7c).*(?=《》)')	// 識別用の正規表現
+				let rubied = text.match(re_rubied)[0]	// re_rubiedにマッチした文字列を取得
+				let emphasis_Text=""
+				for(let i=0; i<rubied.length; i++){	// 1文字ずつ処理
+					// console.log(rubied[i]);
+					let emphasis = ("|"+rubied[i]+"《・》");	// i文字目の1文字に対し、ルビ記号（読み指定に傍点）を付与
+					emphasis_Text+=emphasis	// emphasis_Text に結合（ループで繰り返されることで、文字列の1文字1文字にルビ記号と傍点が付与されるように）
+				}
+				console.log(emphasis_Text)
+				editor.edit(builder => builder.replace(selection, emphasis_Text)) // 選択してた文字列を、newTextに置き換え（カッコを追加）
+				let position = editor.selection.active // カーソル位置の取得
+				let newPosition = position.with(position.line, (editor.selection.start.character+emphasis_Text.length)); // 新しいカーソル位置の定義（文字列の選択開始位置までの文字数 + 選択範囲に記号を付与した文字数-shift）
+				let newSelection = new vscode.Selection(newPosition, newPosition); // 新たな選択位置の定義（範囲が同じ位置,同じ位置 ＝カーソルが動くだけ）
+				editor.selection = newSelection;	// 定義した選択位置を適用
+
 			}else{
-				let newText = ("|"+text+"《》")
+				let newText = ("|"+text+"《》")	// newTextの定義（元文字列にカッコを付与したもの）
 				editor.edit(builder => builder.replace(selection, newText)) // 選択してた文字列を、newTextに置き換え（カッコを追加）
 				let position = editor.selection.active // カーソル位置の取得
 				var shift=1	//カーソル位置をずらす用の変数（通常1、後述で2に変化させる）
